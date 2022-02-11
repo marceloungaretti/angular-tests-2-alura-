ccaf6e9c5e05810c934a359a66e73d2994d397c6
Marcelo <marcelo.ungaretti@gmail.com>
2022-02-10 22:15:00 -0300

    fim da aula 4.6

---
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -0,0 +1,27 @@
+import { Photo } from './interfaces/photo';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-board',
+  templateUrl: './photo-board.component.html',
+  styleUrls: ['./photo-board.component.scss'],
+})
+export class PhotoBoardComponent implements OnChanges {
+  @Input() public photos: Photo[];
+  public rows: any[][] = [];
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.photos) {
+      this.rows = this.groupCloumns(changes.photos.currentValue);
+    }
+  }
+
+  public groupCloumns(photos: Photo[]): any[][] {
+    const newRows = [];
+    const step = 4;
+    for (let index = 0; index < photos.length; index += step) {
+      newRows.push(photos.slice(index, index + step));
+    }
+    return newRows;
+  }
+}
